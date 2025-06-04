d40e76d2d466cbd96bc5cb80cff9159594d306bc
thanachot <thanachotyinkhunthod@gmail.com>
2025-06-05 02:53:37 +0700

    Edit Ui

--- components/UserInterface/BodyInterface.tsx
+++ components/UserInterface/BodyInterface.tsx
@@ -10,14 +10,14 @@ const BodyInterface = () => {
             {/* อันนี้คือหน้าที่ USer เห็นตอน logout ออกมา */}
         <SignedOut>
         <div className="flex flex-col space-y-4 max-w-md p-20 pt-40">
-            <h1 className="text-5xl font-bold text-purple-600">Check-in</h1>
+            <h1 className="text-5xl font-bold text-purple-700">Check-in</h1>
             <h2 className="text-2xl text-purple-400">Check in for class</h2>
             <p className="text-purple-300">
                 To make the lives of students easier<br />
                 when checking in to class.
             </p>
             <SignInButton>
-             <button className="bg-purple-400 text-white font-semibold px-6 py-3 rounded-xl hover:bg-purple-500 transition">
+             <button className="bg-purple-500 text-white text-xl font-bold p-3 rounded-2xl hover:bg-purple-400 transition">
              Start Check-in
                 </button>
             </SignInButton>

ff312991d59db61d0c8661ef83d07ebfe5a99ff4
thanachot <thanachotyinkhunthod@gmail.com>
2025-06-17 15:08:59 +0700

    Edit env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZBPBR1TizP6JtG86f6ir1i6feO3XaMpg",
-  authDomain: "check-in-9a4d0.firebaseapp.com",
-  projectId: "check-in-9a4d0",
-  storageBucket: "check-in-9a4d0.firebasestorage.app",
-  messagingSenderId: "59534542286",
-  appId: "1:59534542286:web:27bef781ac9d45a868579f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);

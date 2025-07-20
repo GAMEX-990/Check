d804d7d69cccfd6ad3348b9579f01833e4e7312c
thanachot <thanachotyinkhunthod@gmail.com>
2025-07-20 15:25:25 +0700

    fix PieChart Come late 1.6(ท้อ)

--- types/SummaryTypes.ts
+++ types/SummaryTypes.ts
@@ -1,6 +1,18 @@
 // สำหรับ Firestore Timestamp หรือ string
 export type FirestoreTimestamp = { toDate: () => Date } | string;
 
+export interface UserAttendance {
+  uid: string;
+  name: string;
+  studentId: string;
+  count: number;
+}
+
+export interface UserData {
+  uid: string;
+  name: string;
+  studentId: string;
+}
 // สำหรับข้อมูลการเช็คชื่อแต่ละคนในแต่ละวัน
 export interface DailyCheckedInUser {
   uid: string;
